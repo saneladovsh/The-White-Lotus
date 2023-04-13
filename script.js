022bc308caacba59e43ece96f00b58da6a476042
Sanela <saneladovlete@yahoo.com>
2023-04-13 15:55:03 +0300

    start

---
+++ script.js
@@ -0,0 +1,17 @@
+gsap.to(" #hero h1", {
+  text: "흰 연꽃",
+  ease: "power1.in",
+  duration: 3,
+});
+
+gsap.to(" #hero h2", {
+  text: "WHITE LOTUS",
+  ease: "power1.in",
+  duration: 3,
+});
+
+gsap.to(" #hero p", {
+  text: "REVOLUTIONARY SKINCARE",
+  ease: "power1.in",
+  duration: 5,
+});
